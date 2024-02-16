5dace004323f087be1f2e96043580af09ec94cfc
Fakhri Adi Saputra <fakhriadisaputra15@gmail.com>
2024-02-17 01:15:56 +0700

    Update

--- src/main/java/id/fakhrads/datapribadi/controller/DataPribadiController.java
+++ src/main/java/id/fakhrads/datapribadi/controller/DataPribadiController.java
@@ -37,11 +37,37 @@ public class DataPribadiController {
         return "tambah";
     }
 
-    @PostMapping("/tambah/simpan")
+    @PostMapping("/edituser/simpan")
     public String createData(DataPribadi data, RedirectAttributes ra) {
         service.save(data);
         ra.addFlashAttribute("message", "The user has been saved successfully.");
         return "redirect:/";
+    }
+
+    @PostMapping("/tambah/simpan")
+    public String editSaveData(DataPribadi data, RedirectAttributes ra) {
+        try {
+            DataPribadi existingData = service.get(data.getNik());
+            ra.addFlashAttribute("error", "NIK already exists. Please use a different one.");
+        } catch (DataPribadiExecption ex) {
+            service.save(data);
+            ra.addFlashAttribute("message", "The user has been saved successfully.");
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/view/{nik}")
+    public String showViewForm(@PathVariable("nik") Long nik, Model model, RedirectAttributes ra) {
+        try {
+            DataPribadi data = service.get(nik);
+            model.addAttribute("data", data);
+            model.addAttribute("pageTitle", "View User (ID: " + nik + ")");
+
+            return "view";
+        } catch (DataPribadiExecption e) {
+            ra.addFlashAttribute("message", e.getMessage());
+            return "redirect:/";
+        }
     }
 
     @GetMapping("/edit/{nik}")
